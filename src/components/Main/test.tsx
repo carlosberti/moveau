a2008355d9f84ab989774337d7b03043bcf00848
Carlos Berti <carlosberti182@gmail.com>
2021-05-11 16:32:20 -0300

    feat: create snapshot test

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -4,8 +4,9 @@ import Main from '.'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
-    render(<Main />)
+    const { container } = render(<Main />)
 
     expect(screen.getByRole('heading', { name: /moveau/i })).toBeInTheDocument()
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
