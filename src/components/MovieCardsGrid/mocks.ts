f26315ce9c673ac75b18e669c4d6d4b65b8ba2a3
Carlos Berti <carlosberti182@gmail.com>
2021-05-12 19:44:13 -0300

    feat: add overview to mock

--- src/components/MovieCardsGrid/mocks.ts
+++ src/components/MovieCardsGrid/mocks.ts
@@ -2,121 +2,169 @@ export const movies = [
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin asdkfbkasjldf lsahdf asjdhf asj '
+    name: 'Aladdin asdkfbkasjldf lsahdf asjdhf asj ',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   },
   {
     id: '1',
     img: '/assets/img/aladdin.jpeg',
-    name: 'Aladdin'
+    name: 'Aladdin',
+    overview:
+      'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
   }
 ]
