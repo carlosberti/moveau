12a83c8e1ad619899ba70d66cc879266f1ec50fe
Carlos Berti <carlosberti182@gmail.com>
2021-05-12 20:25:59 -0300

    refactor: remove movie state from Home

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -3,7 +3,6 @@ import { Container } from 'components/Container'
 import { MovieCardProps } from 'components/MovieCard'
 import MovieCardsGrid from 'components/MovieCardsGrid'
 import MoviePopup from 'components/MoviePopup'
-import { useMovieStore } from 'store'
 import * as s from './styles'
 
 export type HomeTemplateProps = {
@@ -11,11 +10,9 @@ export type HomeTemplateProps = {
 }
 
 const Home = ({ movies }: HomeTemplateProps) => {
-  const movie = useMovieStore((state) => state.movie)
-
   return (
     <Base>
-      {movie && <MoviePopup {...movie} />}
+      <MoviePopup />
       <Container>
         <s.MovieCards>
           <MovieCardsGrid movies={movies} />
--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
-
 import { movies } from 'components/MovieCardsGrid/mocks'
+
 import Home from '.'
 
 jest.mock('components/MovieCardsGrid', () => {
@@ -26,24 +26,6 @@ describe('<Home />', () => {
     render(<Home movies={movies} />)
 
     expect(screen.getByTestId('Mock MovieCardsGrid')).toBeInTheDocument()
-    expect(screen.queryByTestId('Mock MoviePopup')).not.toBeInTheDocument()
-  })
-
-  it('should render popup', () => {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const movie = jest.spyOn(require('store'), 'useMovieStore')
-    movie.mockImplementation(() => ({
-      movies: {
-        id: '1',
-        img: '/assets/img/aladdin.jpeg',
-        name: 'Aladdin asdkfbkasjldf lsahdf asjdhf asj ',
-        overview:
-          'kjahbsf hjksadf jkhasdf kjahsdf jkasdhf jkasdf jkhasd fjhsad fkj,ashdf jklasdhf kjasdhf kjasd fjkasdhf jkasdhf jkahsdf jksahd fjkasd fjha'
-      }
-    }))
-
-    render(<Home movies={movies} />)
-
     expect(screen.queryByTestId('Mock MoviePopup')).toBeInTheDocument()
   })
 })
