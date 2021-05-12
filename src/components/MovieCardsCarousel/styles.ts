ff4164745171fcfe3217c56b1ca2550daca1ce6b
Carlos Berti <carlosberti182@gmail.com>
2021-05-11 22:12:22 -0300

    feat: remove scrollbar code commented

--- src/components/MovieCardsCarousel/styles.ts
+++ src/components/MovieCardsCarousel/styles.ts
@@ -5,6 +5,12 @@ export const Wrapper = styled.section`
   gap: 1.6rem;
   overflow: scroll;
   padding: 2.4rem 0 2.4rem 2.4rem;
+  /* -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  ::-webkit-scrollbar {
+    display: none;
+  } */
 
   > button:last-of-type {
     padding-right: 2.4rem;
