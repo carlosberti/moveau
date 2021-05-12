13807d02ef5f8474ffa1e271cc7ed5f27b99dcca
Carlos Berti <carlosberti182@gmail.com>
2021-05-12 07:21:09 -0300

    fix: remove container padding from carousel

--- src/components/MovieCardsCarousel/styles.ts
+++ src/components/MovieCardsCarousel/styles.ts
@@ -4,7 +4,18 @@ export const Wrapper = styled.section`
   display: flex;
   gap: 1.6rem;
   overflow: scroll;
-  padding: 2.4rem 0 2.4rem 2.4rem;
+  padding: 2.4rem 0 2.4rem 1.2rem;
+  margin-left: -1.2rem;
+
+  @media screen and (min-width: 768px) {
+    padding-left: 2.4rem;
+    margin-left: -2.4rem;
+  }
+
+  @media screen and (min-width: 1168px) {
+    padding-left: 3.2rem;
+    margin-left: -3.2rem;
+  }
   /* -ms-overflow-style: none;
   scrollbar-width: none;
 
