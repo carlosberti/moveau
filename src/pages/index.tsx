7193713898872f1a07586e52817962ece296f030
Carlos Berti <carlosberti182@gmail.com>
2021-05-11 22:16:52 -0300

    feat: add carousel to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,63 @@
-import Main from 'components/Main'
+import { Container } from 'components/Container'
+import MovieCardsCarousel from 'components/MovieCardsCarousel'
+
+const props = {
+  movies: [
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin s.dkjf jasdf ljasd flajsdhf jklasd fjkalsdhf asnkdf aksjldf lasjkdf jkasd fklasdf aljsd '
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    }
+  ]
+}
 
 export default function Home() {
-  return <Main title="Moveau" />
+  return (
+    <Container>
+      <MovieCardsCarousel {...props} />
+    </Container>
+  )
 }
