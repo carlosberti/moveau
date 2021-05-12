f1fe4b25a6749a3fb7a7590d62f0eb986a031c73
Carlos Berti <carlosberti182@gmail.com>
2021-05-11 21:50:39 -0300

    feat: create container

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 138rem;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 1.2rem;
+  padding-right: 1.2rem;
+  background-color: #1d1e26;
+  @media screen and (min-width: 768px) {
+    padding-left: 2.4rem;
+    padding-right: 2.4rem;
+  }
+  @media screen and (min-width: 1168px) {
+    padding-left: 3.2rem;
+    padding-right: 3.2rem;
+  }
+`
