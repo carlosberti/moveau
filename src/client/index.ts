4a2d65dc968e9d7dd7c93e3a22e38d08c3ebd97d
Carlos Berti <carlosberti182@gmail.com>
2021-05-12 13:33:01 -0300

    feat: create axios endpoint

---
+++ src/client/index.ts
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const BASE_MOVIE_URL = 'https://api.themoviedb.org/3/movie'
+
+const config = {
+  headers: {
+    Authorization: `Bearer ${process.env.TOKEN}`,
+    'Content-Type': 'application/json;charset=utf-8'
+  }
+}
+
+type HomeMoviesResponse = {
+  original_title: string
+  poster_path: string
+}
+
+export const getHomeMovies = async (): Promise<HomeMoviesResponse[]> => {
+  const response = await axios.get(`${BASE_MOVIE_URL}/popular`, config)
+
+  return response.data.results
+}
