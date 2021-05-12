a1b1600f139217aa29677847b3fb783ef65ad42e
Carlos Berti <carlosberti182@gmail.com>
2021-05-12 07:17:30 -0300

    feat: add moviecards grid to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,48 @@
 import { Container } from 'components/Container'
 import MovieCardsCarousel from 'components/MovieCardsCarousel'
+import MovieCardsGrid from 'components/MovieCardsGrid'
 
 const props = {
   movies: [
     {
       img: '/assets/img/aladdin.jpeg',
       name: 'Aladdin s.dkjf jasdf ljasd flajsdhf jklasd fjkalsdhf asnkdf aksjldf lasjkdf jkasd fklasdf aljsd '
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
+    },
+    {
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin'
     },
     {
       img: '/assets/img/aladdin.jpeg',
@@ -57,6 +94,7 @@ const props = {
 export default function Home() {
   return (
     <Container>
+      <MovieCardsGrid {...props} />
       <MovieCardsCarousel {...props} />
     </Container>
   )
