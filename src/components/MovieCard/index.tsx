d41a39e33b9e745d78156e5adb6306378d002067
Carlos Berti <carlosberti182@gmail.com>
2021-05-13 00:02:25 -0300

    feat: add overview and handle click

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 
+import { useMovieStore } from 'store'
+import { getVideos } from 'client'
 import * as s from './styles'
 
 export type MovieCardProps = {
   id: string
   img: string
   name: string
+  overview: string
   width?: number
   height?: number
 }
@@ -14,14 +17,39 @@ const MovieCard = ({
   id,
   img,
   name,
+  overview,
   width = 131,
   height = 188
 }: MovieCardProps) => {
-  console.log(id)
+  const addMovie = useMovieStore((state) => state.addMovie)
+
+  const handleClick = async () => {
+    const response = await getVideos({ id })
+
+    const trailers = response.filter(
+      (video) => video.type.match(/trailer/i) && video.site.match(/youtube/i)
+    )
+
+    const movies = {
+      overview,
+      id,
+      name,
+      img,
+      video: trailers.length > 0 && trailers[0].key
+    }
+
+    addMovie(movies)
+  }
 
   return (
     <s.Wrapper>
-      <Image src={img} alt={name} width={width} height={height} />
+      <Image
+        src={img}
+        alt={name}
+        width={width}
+        height={height}
+        onClick={handleClick}
+      />
       <p>{name}</p>
     </s.Wrapper>
   )
