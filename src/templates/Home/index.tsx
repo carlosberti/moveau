bdef57370695f3fc24acece0a979838b9d485cfd
Carlos Berti <carlosberti182@gmail.com>
2021-05-13 01:06:41 -0300

    feat: add infinite scroll to homepage

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -4,18 +4,59 @@ import { MovieCardProps } from 'components/MovieCard'
 import MovieCardsGrid from 'components/MovieCardsGrid'
 import MoviePopup from 'components/MoviePopup'
 import * as s from './styles'
+import { useEffect, useRef, useState } from 'react'
+import { getHomeMovies } from 'client'
+import { homeMoviesMapper } from 'utils/mappers'
 
 export type HomeTemplateProps = {
   movies: MovieCardProps[]
 }
 
 const Home = ({ movies }: HomeTemplateProps) => {
+  const [page, setPage] = useState(1)
+  const [homeMovies, setHomeMovies] = useState(movies)
+  const loader = useRef(null)
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '50px',
+      threshold: 0.5
+    }
+    const observer = new IntersectionObserver(handleObserver, options)
+    if (loader.current) {
+      observer.observe(loader.current!)
+    }
+  }, [])
+
+  useEffect(() => {
+    async function getMoreMovies() {
+      try {
+        const movies = await getHomeMovies({ page })
+        const newMovies = homeMoviesMapper(movies)
+        setHomeMovies((homeMovies) => [...homeMovies, ...newMovies])
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getMoreMovies()
+  }, [page])
+
+  const handleObserver = (entities: IntersectionObserverEntry[]) => {
+    const target = entities[0]
+    if (target.isIntersecting) {
+      setPage((page) => page + 1)
+    }
+  }
+
   return (
     <Base>
       <MoviePopup />
       <Container>
         <s.MovieCards>
-          <MovieCardsGrid movies={movies} />
+          <MovieCardsGrid movies={homeMovies} />
+          <s.LoadingSpinner ref={loader} />
         </s.MovieCards>
       </Container>
     </Base>
--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -1,3 +1,23 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
-export const MovieCards = styled.main``
+export const MovieCards = styled.main`
+  display: flex;
+  flex-direction: column;
+`
+
+const spinningAnimation = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+export const LoadingSpinner = styled.div`
+  margin-top: 6.4rem;
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+  border: 0.4rem solid #eee;
+  border-left-color: #1d1e26;
+  animation: ${spinningAnimation} 0.5s linear infinite;
+  align-self: center;
+`
