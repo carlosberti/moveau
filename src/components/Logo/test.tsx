aa6a2fba64531ca57cb96d43ae9c529ddf1a91bf
Carlos Berti <carlosberti182@gmail.com>
2021-05-12 23:47:17 -0300

    test: create logo

---
+++ src/components/Logo/test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+
+import Logo from '.'
+
+describe('<Logo />', () => {
+  it('should render correctly', () => {
+    render(<Logo />)
+
+    expect(screen.getByLabelText(/movieau/i)).toBeInTheDocument()
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    render(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/movieau/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: 'screen and (max-width: 600px)'
+      }
+    )
+  })
+})
