4e93598b3bba06c56cb1f96d10bf2d6ad951cee0
Carlos Berti <carlosberti182@gmail.com>
2021-05-14 02:29:42 -0300

    feat: add themeprovider to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,13 @@
 import '../.jest/next-image.mock'
+import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
+import {darkTheme} from 'styles/theme'
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={darkTheme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
