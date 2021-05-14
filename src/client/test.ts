0730428613101a1af670a4bacc0f40d9d4cde357
Carlos Berti <carlosberti182@gmail.com>
2021-05-14 17:00:09 -0300

    feat: use toStricEqual instead of toEqual

--- src/client/test.ts
+++ src/client/test.ts
@@ -6,7 +6,7 @@ describe('getHomeMovies()', () => {
   it('should getHomeMovies', async () => {
     const response = await getHomeMovies({ page: 1 })
 
-    expect(response).toEqual([
+    expect(response).toStrictEqual([
       {
         id: '1',
         title: 'req_title',
@@ -21,7 +21,7 @@ describe('getVideos()', () => {
   it('should getVideos', async () => {
     const response = await getVideos({ id: '1' })
 
-    expect(response).toEqual([
+    expect(response).toStrictEqual([
       {
         key: 'req_key',
         site: 'req_site',
@@ -35,7 +35,7 @@ describe('getMovieDetails()', () => {
   it('should getMovieDetails', async () => {
     const response = await getMovieDetails({ id: '1' })
 
-    expect(response).toEqual({
+    expect(response).toStrictEqual({
       title: 'req_title',
       overview: 'req_overview',
       images: [
@@ -58,7 +58,7 @@ describe('getSearchMovies()', () => {
   it('should getSearchMovies', async () => {
     const response = await getSearchMovies({ query: 'any_query', page: 1 })
 
-    expect(response).toEqual({
+    expect(response).toStrictEqual({
       results: [
         {
           id: '1',
