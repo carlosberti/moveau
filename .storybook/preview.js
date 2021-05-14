a15a9f1004c53d71dee25443e605e86e078803dc
Carlos Berti <carlosberti182@gmail.com>
2021-05-14 02:45:02 -0300

    feat: add movie to state on storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,12 +2,24 @@ import '../.jest/next-image.mock'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import {darkTheme} from 'styles/theme'
+import {useMovieStore} from 'store'
 
 export const decorators = [
-  (Story) => (
-    <ThemeProvider theme={darkTheme}>
-      <GlobalStyles />
-      <Story />
-    </ThemeProvider>
-  )
+  (Story) => {
+    const addMovie = useMovieStore(state => state.addMovie)
+    addMovie({
+      id: '278',
+      img: '/assets/img/aladdin.jpeg',
+      name: 'Aladdin',
+      overview: ',jahsgdf asldjfh adjlsfh asjldfh asldjfh asdhjlf lasjdh',
+      video: 'ur9GKLl8v4U'
+    })
+
+    return (
+      <ThemeProvider theme={darkTheme}>
+        <GlobalStyles />
+        <Story />
+      </ThemeProvider>
+    )
+  }
 ]
