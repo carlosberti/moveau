25c219a748792110e472ec9bf1bdc37cd1110d73
Carlos Berti <carlosberti182@gmail.com>
2021-05-14 16:56:45 -0300

    tes: add card click test

--- src/components/MovieCard/test.tsx
+++ src/components/MovieCard/test.tsx
@@ -1,7 +1,9 @@
+import 'server.mock'
 import 'next-image.mock'
-import { render, screen } from 'utils/test-utils'
+import { render, screen, waitFor } from 'utils/test-utils'
 
 import MovieCard from '.'
+import userEvent from '@testing-library/user-event'
 
 const props = {
   id: '1',
@@ -16,8 +18,21 @@ describe('<MovieCard />', () => {
 
     const img = screen.getByRole('img', { name: 'Aladdin' })
 
+    expect(screen.getByLabelText(/click to open/i)).toBeInTheDocument()
     expect(img).toBeInTheDocument()
     expect(img).toHaveAttribute('src', '/any_path')
     expect(screen.getByText(/aladdin/i)).toBeInTheDocument()
+  })
+
+  it('should add movie onClick', async () => {
+    render(<MovieCard {...props} />)
+
+    const card = screen.getByLabelText(/click to open/i)
+
+    expect(card.getAttribute('tabIndex')).toBe('0')
+
+    userEvent.click(card)
+
+    await waitFor(() => expect(card.getAttribute('tabIndex')).toBe('-1'))
   })
 })
