642dcbb0f7857b627237cb78459a6271a97c4130
Carlos Berti <carlosberti182@gmail.com>
2021-05-14 16:57:37 -0300

    feat: add search option to Home page

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,27 +1,32 @@
+import { ChangeEvent, useEffect, useRef, useState } from 'react'
+import slugify from 'slugify'
+
 import Base from 'templates/Base'
 import { Container } from 'components/Container'
 import { MovieCardProps } from 'components/MovieCard'
 import MovieCardsGrid from 'components/MovieCardsGrid'
 import MoviePopup from 'components/MoviePopup'
-import * as s from './styles'
-import { useEffect, useRef, useState } from 'react'
-import { getHomeMovies } from 'client'
+import { getHomeMovies, getSearchMovies } from 'client'
 import { homeMoviesMapper } from 'utils/mappers'
+import * as s from './styles'
 
 export type HomeTemplateProps = {
   movies: MovieCardProps[]
 }
 
 const Home = ({ movies }: HomeTemplateProps) => {
-  const [page, setPage] = useState(1)
-  const [homeMovies, setHomeMovies] = useState(movies)
+  const [page, setPage] = useState(2)
+  const [moviesToShow, setMoviesToShow] = useState<MovieCardProps[]>(movies)
+  const [search, setSearch] = useState<string | undefined>(undefined)
+  const [totalPages, setTotalPages] = useState<number | undefined>(undefined)
+  const timer = useRef<NodeJS.Timeout | undefined>()
   const loader = useRef(null)
 
   useEffect(() => {
     const options = {
       root: null,
       rootMargin: '50px',
-      threshold: 0.5
+      threshold: 1
     }
     const observer = new IntersectionObserver(handleObserver, options)
     if (loader.current) {
@@ -29,18 +34,30 @@ const Home = ({ movies }: HomeTemplateProps) => {
     }
   }, [])
 
+  const moviesToGet = async () =>
+    search
+      ? page <= totalPages! &&
+        (await getSearchMovies({ query: slugify(search), page: page })).results
+      : await getHomeMovies({ page })
+
   useEffect(() => {
+    let isMounted = true
     async function getMoreMovies() {
       try {
-        const movies = await getHomeMovies({ page })
-        const newMovies = homeMoviesMapper(movies)
-        setHomeMovies((homeMovies) => [...homeMovies, ...newMovies])
+        const movies = await moviesToGet()
+        const newMovies = movies ? homeMoviesMapper(movies) : []
+        if (isMounted)
+          setMoviesToShow((moviesToShow) => [...moviesToShow, ...newMovies])
       } catch (error) {
         console.log(error)
       }
     }
 
     getMoreMovies()
+
+    return () => {
+      isMounted = false
+    }
   }, [page])
 
   const handleObserver = (entities: IntersectionObserverEntry[]) => {
@@ -50,13 +67,48 @@ const Home = ({ movies }: HomeTemplateProps) => {
     }
   }
 
+  const handleInputChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.currentTarget
+
+    clearTimeout(timer.current as NodeJS.Timeout)
+
+    timer.current = setTimeout(async () => {
+      if (value !== '') {
+        const response = await getSearchMovies({
+          query: slugify(value),
+          page: 1
+        })
+        setSearch(value)
+        setPage(2)
+        setMoviesToShow(homeMoviesMapper(response.results))
+        setTotalPages(response.total_pages)
+        return
+      }
+      setSearch(undefined)
+      setTotalPages(undefined)
+      setPage(2)
+      setMoviesToShow(movies)
+    }, 1000)
+  }
+
+  const loadingSpinner = () =>
+    (!totalPages || page <= totalPages) && (
+      <s.LoadingSpinner role="img" ref={loader} aria-label="Loading more" />
+    )
+
   return (
     <Base>
       <MoviePopup />
       <Container>
+        <s.Input
+          placeholder="Search for movies"
+          onChange={handleInputChange}
+          // eslint-disable-next-line jsx-a11y/no-autofocus
+          autoFocus
+        />
         <s.MovieCards>
-          <MovieCardsGrid movies={homeMovies} />
-          <s.LoadingSpinner ref={loader} />
+          <MovieCardsGrid movies={moviesToShow} />
+          {loadingSpinner()}
         </s.MovieCards>
       </Container>
     </Base>
--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -1,8 +1,37 @@
 import styled, { css, keyframes } from 'styled-components'
 
+export const Input = styled.input`
+  ${({ theme }) => css`
+    display: block;
+    margin: 1.2rem auto;
+    width: 80%;
+    height: 4.3rem;
+    max-width: 40rem;
+    line-height: 1;
+    font-size: 2.4rem;
+    padding: 0.8rem;
+    background-color: transparent;
+    border: 0.1rem solid;
+    border-image: ${theme.colors.border.poster};
+    outline: none;
+    color: ${theme.colors.posterText.normal};
+
+    &:hover,
+    &:focus {
+      border-image: ${theme.colors.border.wallpaper};
+    }
+
+    @media screen and (min-width: 768px) {
+      margin-top: 0;
+      height: 5.3rem;
+    }
+  `}
+`
+
 export const MovieCards = styled.main`
   display: flex;
   flex-direction: column;
+  margin-top: 5.2rem;
 `
 
 const spinningAnimation = keyframes`
