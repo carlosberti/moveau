b1463a3348cc6b6c37d444c8aed16ac0ddcbbb60
Carlos Berti <carlosberti182@gmail.com>
2021-05-14 17:12:00 -0300

    test: add mappers tests

---
+++ src/utils/mappers/test.ts
@@ -0,0 +1,82 @@
+import { HomeMoviesResponse, MovieDetailsResponse } from 'client'
+import { homeMoviesMapper, movieDetailsMapper } from '.'
+
+describe('homeMoviesMapper()', () => {
+  it('should return the right format when mapped', () => {
+    const movies: HomeMoviesResponse[] = [
+      {
+        id: '1',
+        poster_path: 'any_path',
+        title: 'any_title',
+        overview: 'any_overview'
+      }
+    ]
+
+    expect(homeMoviesMapper(movies)).toStrictEqual([
+      {
+        id: '1',
+        img: 'https://image.tmdb.org/t/p/w300/any_path',
+        name: 'any_title',
+        overview: 'any_overview'
+      }
+    ])
+  })
+
+  it('should return not-found image when poster_path is not provided', () => {
+    const movies = [
+      {
+        id: '1',
+        title: 'any_title',
+        overview: 'any_overview'
+      }
+    ]
+
+    expect(homeMoviesMapper(movies as HomeMoviesResponse[])).toStrictEqual([
+      {
+        id: '1',
+        img: '/assets/img/not-found.jpg',
+        name: 'any_title',
+        overview: 'any_overview'
+      }
+    ])
+  })
+})
+
+describe('movieDetailsMapper()', () => {
+  it('should return the right format when mapped', () => {
+    const movie: MovieDetailsResponse = {
+      title: 'any_title',
+      overview: 'any_overview',
+      images: [
+        {
+          path: 'any_path'
+        }
+      ],
+      videos: [
+        {
+          key: 'any_key',
+          type: 'any_type',
+          site: 'any_site'
+        }
+      ]
+    }
+
+    expect(movieDetailsMapper({ id: '1', movie })).toStrictEqual({
+      id: '1',
+      name: 'any_title',
+      overview: 'any_overview',
+      images: [
+        {
+          path: 'any_path'
+        }
+      ],
+      videos: [
+        {
+          key: 'any_key',
+          type: 'any_type',
+          site: 'any_site'
+        }
+      ]
+    })
+  })
+})
