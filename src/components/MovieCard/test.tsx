d10028607eb89a8fe63eda25bc8a6c45150a63e1
Carlos Berti <carlosberti182@gmail.com>
2021-05-15 19:32:51 -0300

    test: get data from new stores on movie card

--- src/components/MovieCard/test.tsx
+++ src/components/MovieCard/test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import 'server.mock'
 import 'next-image.mock'
 import { render, screen, waitFor } from 'utils/test-utils'
@@ -34,5 +35,46 @@ describe('<MovieCard />', () => {
     userEvent.click(card)
 
     await waitFor(() => expect(card.getAttribute('tabIndex')).toBe('-1'))
+  })
+
+  it('should add to watchlater store on watch later button click', async () => {
+    const useWatchLaterStore = jest.spyOn(
+      require('store'),
+      'useWatchLaterStore'
+    )
+
+    const setWatchLater = jest.fn()
+
+    useWatchLaterStore.mockImplementation(() => ({
+      setWatchLater,
+      isWatchLater: jest.fn(() => true)
+    }))
+
+    render(<MovieCard {...props} />)
+
+    const watchLaterButton = screen.getByLabelText(/Click to watch later/i)
+
+    userEvent.click(watchLaterButton)
+
+    expect(setWatchLater).toHaveBeenCalled()
+  })
+
+  it('should add to favourite store on favourite button click', async () => {
+    const useFavouriteStore = jest.spyOn(require('store'), 'useFavouriteStore')
+
+    const setFavourite = jest.fn()
+
+    useFavouriteStore.mockImplementation(() => ({
+      setFavourite,
+      isFavourite: jest.fn(() => true)
+    }))
+
+    render(<MovieCard {...props} />)
+
+    const FavouriteButton = screen.getByLabelText(/Click to favourite/i)
+
+    userEvent.click(FavouriteButton)
+
+    expect(setFavourite).toHaveBeenCalled()
   })
 })
