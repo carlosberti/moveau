122ece1d91d8606a46d8be2a2dc1710c52d96322
Carlos Berti <carlosberti182@gmail.com>
2021-05-15 19:28:22 -0300

    feat: add watchlater and favourite store to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
+import { MovieCardProps } from 'components/MovieCard'
 import create from 'zustand'
 
 export type MoviePopupProps = {
@@ -20,21 +21,70 @@ export const useMovieStore = create<MovieStore>((set) => ({
   removeMovie: () => set({ movie: undefined })
 }))
 
-type Movie = {
-  id: string
-  name: string
-  img: string
-}
-
-type FavouriteStore = {
-  favourites: [] | Movie[]
-  addFavourite: (favourite: Movie) => void
+export type FavouriteStore = {
+  items: undefined | MovieCardProps[]
+  isOpen: boolean
+  setItems: (favourite: MovieCardProps) => void
+  clearItems: () => void
+  setIsOpen: () => void
+  isItem: (id: string) => boolean
 }
 
 export const useFavouriteStore = create<FavouriteStore>((set, get) => ({
-  favourites: [],
-  addFavourite: (favourite: Movie) => {
-    const { favourites } = get()
-    set({ favourites: [...favourites, favourite] })
+  items: undefined,
+  isOpen: false,
+  setItems: (favourite: MovieCardProps) => {
+    const { items, isItem } = get()
+
+    const newFavourites = items
+      ? isItem(favourite.id)
+        ? items.filter((item) => item.id !== favourite.id)
+        : [...items, favourite]
+      : [favourite]
+
+    set({ items: [...newFavourites] })
+  },
+  clearItems: () => set({ items: undefined }),
+  setIsOpen: () => set((state) => ({ isOpen: !state.isOpen })),
+  isItem: (id: string): boolean => {
+    const { items } = get()
+
+    const isFavourite = items?.find((item) => item.id === id)
+
+    return !!isFavourite
+  }
+}))
+
+export type WatchLaterStore = {
+  items: undefined | MovieCardProps[]
+  isOpen: boolean
+  setItems: (watchLater: MovieCardProps) => void
+  clearItems: () => void
+  setIsOpen: () => void
+  isItem: (id: string) => boolean
+}
+
+export const useWatchLaterStore = create<WatchLaterStore>((set, get) => ({
+  items: undefined,
+  isOpen: false,
+  setItems: (watchLater: MovieCardProps) => {
+    const { items, isItem } = get()
+
+    const newWatchLater = items
+      ? isItem(watchLater.id)
+        ? items.filter((item) => item.id !== watchLater.id)
+        : [...items, watchLater]
+      : [watchLater]
+
+    set({ items: [...newWatchLater] })
+  },
+  clearItems: () => set({ items: undefined }),
+  setIsOpen: () => set((state) => ({ isOpen: !state.isOpen })),
+  isItem: (id: string): boolean => {
+    const { items } = get()
+
+    const isFavourite = items?.find((item) => item.id === id)
+
+    return !!isFavourite
   }
 }))
