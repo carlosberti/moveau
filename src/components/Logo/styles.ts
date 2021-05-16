303201d6fac1fe0181d44a49f2ca11442a4ec643
Carlos Berti <carlosberti182@gmail.com>
2021-05-16 20:01:40 -0300

    feat: animate i dot

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -17,5 +17,28 @@ export const Wrapper = styled.div`
       stroke: ${theme.colors.white};
       fill: ${theme.colors.white};
     }
+
+    .dot {
+      animation: bounce 2s infinite forwards;
+      transform-origin: bottom;
+
+      @keyframes bounce {
+        0% {
+          transform: translateY(0);
+        }
+        30% {
+          transform: translateY(-0.2rem);
+        }
+        45% {
+          transform: translateY(0);
+        }
+        75% {
+          transform: translateY(-0.1rem);
+        }
+        100% {
+          transform: translateY(0);
+        }
+      }
+    }
   `}
 `
