72f00cd656301b0e93a809bf9bf0bee8fc4a6f15
Carlos Berti <carlosberti182@gmail.com>
2021-05-16 21:24:10 -0300

    feat: create localstorage util

---
+++ src/utils/localStorage/index.ts
@@ -0,0 +1,19 @@
+import { MovieCardProps } from 'components/MovieCard'
+
+const APP_KEY = 'MOVIEAU'
+
+export function getStorageItem(key: string) {
+  if (typeof window === 'undefined') return
+
+  const data = window.localStorage.getItem(`${APP_KEY}_${key}`)
+
+  return JSON.parse(data!)
+}
+
+export function setStorageItem(key: string, value: MovieCardProps[] | []) {
+  if (typeof window === 'undefined') return
+
+  const data = JSON.stringify(value)
+
+  return window.localStorage.setItem(`${APP_KEY}_${key}`, data)
+}
